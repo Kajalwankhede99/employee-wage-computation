1616f58fe722672d9dc1664e925e3b9ab4ba8c65
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-09 12:48:04 +0530

    Welcome to Employee wage computation

---
+++ src/main/java/com/bridgelabz/empwage/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.empwage;
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wage Computation");
+    }
+}
