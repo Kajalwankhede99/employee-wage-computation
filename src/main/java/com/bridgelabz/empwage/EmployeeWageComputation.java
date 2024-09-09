cbcf0be16e35dbd23db5b1e378bef16ebf8a1e28
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-09 14:06:51 +0530

    Uc4- Solving using switch case statement

--- src/main/java/com/bridgelabz/empwage/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/empwage/EmployeeWageComputation.java
@@ -2,28 +2,33 @@ package com.bridgelabz.empwage;
 
 public class EmployeeWageComputation {
 
-    public static void main(String[] args) {
-        int fullTime = 1;
-        int partTime = 2;
+    public static final int fullTime = 1;
+    public static final int partTime=2;
+    public static final int empRatePerHour=20;
+
+   public static void main(String[] args) {
         int empHour = 0;
-        int empRatePerHour=20;
         int empWage = 0;
         System.out.println("Welcome to the Employee Wage Computation");
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == partTime) {
-            System.out.println("Employee is Part time.");
-            empHour = 4;
-        }
-        else if (empCheck == fullTime) {
-            System.out.println("Employee is Full time.");
-            empHour = 8;
-        }
-        else{
-            empHour = 0;
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        switch (empCheck) {
+            case partTime:
+                empHour = 4;
+                System.out.println("Employee is Part time.");
+                break;
+
+            case fullTime:
+                empHour = 8;
+                System.out.println("Employee is Full time.");
+                break;
+            default:
+                System.out.println("Absent");
+                break;
         }
         empWage=empHour*empRatePerHour;
         System.out.println("Employee Daily Wage is: "+empWage);
     }
 }
+
 
 
