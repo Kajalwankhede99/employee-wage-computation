967ad60400c1609a9cfea0dce5e5fbb7b61400bc
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-09 13:46:15 +0530

    Uc2-Calculate Daily Employee Wage

--- src/main/java/com/bridgelabz/empwage/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/empwage/EmployeeWageComputation.java
@@ -4,14 +4,21 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         int fullTime = 1;
+        int empRatePerHour=20;
+        int empHour=0;
+        int empWage=0;
         System.out.println("Welcome to the Employee Wage Computation");
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == fullTime) {
             System.out.println("Employee Present!");
+            empHour = 8;
         } else {
             System.out.println("Employee Absent!");
+            empHour = 0;
 
         }
+        empWage=empHour*empRatePerHour;
+        System.out.println("Employee Daily Wage is: "+empWage);
     }
 }
 
