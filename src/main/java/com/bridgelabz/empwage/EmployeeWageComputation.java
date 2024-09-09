c5ab9c3ede90e55d55ca0ada6340f061714c2775
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-09 12:56:54 +0530

    Uc1- Check Employee is present or absent

--- src/main/java/com/bridgelabz/empwage/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/empwage/EmployeeWageComputation.java
@@ -3,6 +3,16 @@ package com.bridgelabz.empwage;
 public class EmployeeWageComputation {
 
     public static void main(String[] args) {
+        int fullTime = 1;
         System.out.println("Welcome to the Employee Wage Computation");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == fullTime) {
+            System.out.println("Employee Present!");
+        } else {
+            System.out.println("Employee Absent!");
+
+        }
     }
 }
+
+
